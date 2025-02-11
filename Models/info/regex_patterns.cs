ff682c1de2a4d57b703f6de64d9e915eb6ad4510
dusk5archiy <dusqdn.eml@gmail.com>
2025-02-11 22:46:23 +0700

    remove info_strings.cs, as it is deprecated

--- Models/info/regex_patterns.cs
+++ Models/info/regex_patterns.cs
@@ -4,14 +4,17 @@ using System.Text.RegularExpressions;
 
 class RegexPatterns
 {
+    // ========================================================================
     public static string username = "^[a-zA-Z0-9_]{3,16}$";
     public static string password = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^\\da-zA-Z]).{8,}$";
     public static string tel = @"^\d{10-11}$";
 
+    // ========================================================================
     public static bool match(string str, string pattern)
     {
         return Regex.IsMatch(str, pattern);
     }
+    // ========================================================================
 }
 
 /* EOF */
