a187529df39c8b14476b7df54f1c4484c50d6f33
dusk5archiy <dusqdn.eml@gmail.com>
2025-03-13 07:38:14 +0700

    bug: teacher thumbnail

--- Controllers/legacy/HomeController.cs
+++ Controllers/legacy/HomeController.cs
@@ -1,6 +1,5 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Data.SqlClient;
 using REPO.Models;
 
 namespace REPO.Controllers;
@@ -17,30 +16,7 @@ public class HomeController : Controller
     public IActionResult Index()
     {
         List<Dictionary<string, string>> teacher_dicts = new();
-        void func(SqlDataReader reader)
-        {
-            Teacher teacher = DataReader.get_data_obj<Teacher>(reader);
-            Dictionary<string, string> dict = teacher.to_dict();
-            teacher_dicts.Add(dict);
-        }
-
-        void conn_func(SqlConnection conn)
-        {
-            Query q = new(Table.teacher);
-            q.select(conn, func);
-            for (int i = 0; i < teacher_dicts.Count; i++)
-            {
-                Query q2 = new(Table.teacher_subject);
-                q2.join(Field.subject__id, Field.teacher_subject__sbj_id);
-                q2.where_(Field.teacher_subject__tch_id, int.Parse(teacher_dicts[i]["id"]));
-                q2.output(Field.subject__name);
-                List<string> subjects = q2.select(conn);
-                teacher_dicts[i]["subjects"] = string.Join(", ", subjects);
-            }
-        }
-
-        Database.exec(conn_func);
-
+        Database.exec(conn => teacher_dicts = Test2.demo(conn));
         ViewBag.teachers = teacher_dicts;
         Console.WriteLine($"Fetched {teacher_dicts.Count} teachers");
         return View();
