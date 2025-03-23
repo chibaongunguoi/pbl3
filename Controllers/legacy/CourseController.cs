c07049fa2126cc9a38aec9dbcc0cb1d1b42c58d2
chibaongunguoi <chibaongunguoi@gmail.com>
2025-03-23 10:39:37 +0700

    xong thanh navbar dưới và thêm Controllers

---
+++ Controllers/legacy/CourseController.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using REPO.Models;
+
+namespace REPO.Controllers;
+
+public class CourseController : Controller
+{
+    private readonly ILogger<CourseController> _logger;
+
+    public CourseController(ILogger<CourseController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        List<Dictionary<string, string>> teacher_dicts = new();
+        Database.exec(conn => teacher_dicts = Test2.demo(conn));
+        ViewBag.teachers = teacher_dicts;
+        Console.WriteLine($"Fetched {teacher_dicts.Count} teachers");
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(
+            new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier }
+        );
+    }
+}
---
+++ Controllers/legacy/TeacherController.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using REPO.Models;
+
+namespace REPO.Controllers;
+
+public class TeacherController : Controller
+{
+    private readonly ILogger<TeacherController> _logger;
+
+    public TeacherController(ILogger<TeacherController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        List<Dictionary<string, string>> teacher_dicts = new();
+        Database.exec(conn => teacher_dicts = Test2.demo(conn));
+        ViewBag.teachers = teacher_dicts;
+        Console.WriteLine($"Fetched {teacher_dicts.Count} teachers");
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(
+            new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier }
+        );
+    }
+}
